4823b88564a5dc47e1f240a2dac4c4e12f25bf1a
Kamil Cholipski <kamil.cholipski@gmail.com>
2022-01-30 23:04:36 +0100

    Remove field method from GoogleController.php

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -27,7 +27,7 @@ class GoogleController extends Controller
     public function callbackFromGoogle()
     {
         try {
-            $user = Socialite::driver('google')->fields(['name', 'first_name', 'last_name', 'email', 'gender', 'verified'])->user();
+            $user = Socialite::driver('google')->user();
 
             $is_user = User::where('email', $user->getEmail())->first();
             if(!$is_user){
