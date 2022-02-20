8358fbf6c97699ba00bba9cd6259b97c38793b78
Kamil Cholipski <kamil.cholipski@gmail.com>
2022-02-20 18:47:43 +0100

    Fix toggle checkbox on edit question page

--- resources/js/Pages/Question/Edit.vue
+++ resources/js/Pages/Question/Edit.vue
@@ -49,12 +49,16 @@
 
                                 {{ String.fromCharCode(65 + k) }}
                                 </span>
-                                <input type="text" id="website-admin"
+                                <textarea id="website-admin"
                                        class="rounded-none rounded-r-lg bg-gray-50 w-1/2 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                        placeholder=""
                                        v-model="input.answer"
                                        :class="{'border-red-500 bg-red-200':input.deleted}"/>
                             </div>
+                            <input type="radio" name="correct_answer" v-model="input.is_correct" v-bind:value="true" v-on:click="changeCorrectAnswer(input)" class="toggle mt-2">
+                            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300 mt-2">
+                                {{input.is_correct ? "TAK" : "NIE"}}
+                            </span>
                         </div>
                         <div class="flex items-center justify-end mt-4 flex-col-reverse gap-2 sm:flex-row">
                             <Button class="text-white w-full bg-gray-800 hover:bg-gray-900 focus:ring-4 mt-12 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800 dark:border-gray-700" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
@@ -147,11 +151,19 @@ export default {
         },
         undo(input) {
             input.deleted = false
+        },
+        changeCorrectAnswer(input)
+        {
+            this.form.answers.forEach((item) =>{ item.is_correct = false});
         }
     }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+[type='radio']:checked
+{
+    background-image: none;
+    background: black;
+}
 </style>
