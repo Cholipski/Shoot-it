f5951d90b8fa4c3426a77692cb5d0aeb90b3ab00
Kamil Cholipski <kamil.cholipski@gmail.com>
2022-03-01 11:01:28 +0100

    Add new buttons for ModalActiveExam.vue

--- resources/js/Components/ModalActiveExam.vue
+++ resources/js/Components/ModalActiveExam.vue
@@ -6,9 +6,10 @@
             <div class="modal-box">
                 <h3 class="font-bold text-black text-lg">Egzamin w trakcie</h3>
                 <p class="py-4 text-black">Wygląda na to że jeden egzamin jest jeszcze aktywny, chciałbyś do niego powrócić ?</p>
-                <div class="modal-action">
-                    <a href="#" class="btn">TAK</a>
-                    <a href="#" class="btn">NIE</a>
+                <div class="flex flex-col justify-center gap-2">
+                    <a href="#" class="btn">Powróć do egzaminu</a>
+                    <a href="#" class="btn">Zakończ egzamin i rozpocznij nowy</a>
+                    <a href="#" class="btn">Anuluj</a>
 
                 </div>
             </div>
