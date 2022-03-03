7ac7895e52db63990b90de32a4e3757f75d8f63b
Kamil Cholipski <kamil.cholipski@gmail.com>
2022-03-03 12:37:45 +0100

    Add route for profile

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -10,7 +10,6 @@
                     <div class="h-screen drawer w-full">
                         <input id="my-drawer-3" type="checkbox" class="drawer-toggle">
                         <div class="flex flex-col drawer-content">
-                            <!-- Navbar -->
                             <div class="w-full flex justify-between md:justify-end navbar mb-2 shadow-lg bg-base-100 text-gray-400">
                                 <div class="flex-none md:hidden">
                                     <label for="my-drawer-3" class="btn btn-square btn-ghost">
@@ -18,14 +17,14 @@
                                     </label>
                                 </div>
                                 <div class="flex-none">
-                                    <!--                                <notifications />-->
+<!--                                                                    <notifications />-->
                                     <div class="dropdown dropdown-end">
                                         <div tabindex="0" class="m-1 mr-4">
-                                            <svg class="w-7 h-7 stroke-cyan-700" fill="none" stroke="black" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+                                            <svg class="w-7 h-7" fill="none" stroke="black" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
                                         </div>
                                         <ul tabindex="0" class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
                                             <li>
-                                                <Link class="logout flex items-center gap-2">
+                                                <Link :href="route('profile.index')" class="logout flex items-center gap-2">
                                                     <i class="logout-icon fas fa-user text-gray-600 text-lg cursor-pointer" />
                                                     <span>Profil</span>
                                                 </Link>
